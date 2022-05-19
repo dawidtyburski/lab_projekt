38e88e324ccfe62a198f84bd77ae60823f8a1e1e
Dawid Tyburski <Dawid.Tyburski@microsoft.wsei.edu.pl>
2022-05-19 20:37:39 +0200

    190520221

--- lab_projekt/MainWindow.xaml.cs
+++ lab_projekt/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace lab_projekt
 {
     /// <summary>
@@ -23,6 +24,40 @@ namespace lab_projekt
         public MainWindow()
         {
             InitializeComponent();
+            MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+
+            
+        }
+        void OnClick1(object sender, RoutedEventArgs e)
+        {
+            string l = Login.Text;
+            string p = Password.Password;
+            ProjektDbContext db = new ProjektDbContext();
+            var users = from u in db.Users
+                        select u;
+            foreach (var item in users)
+            {
+                if (l == item.Username)
+                {
+                    if (p == item.Password)
+                    {
+                        Login.Text = "Zalogowano!!";
+                    }
+                    else break;
+
+                }
+                else break;
+
+            }
+
+        }
+        void OnClick2(object sender, RoutedEventArgs e)
+        {
+
+        }
+        void OnClick3(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ lab_projekt/ProjektDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace lab_projekt
+{
+    class ProjektDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=(localdb)\mssqllocaldb;database=ProjektDb;trusted_connection=true;");
+        }
+    }
+}
