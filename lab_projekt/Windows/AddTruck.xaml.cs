5487238c478f64b9312d0f223db3e46a4d1ca429
Dawid Tyburski <Dawid.Tyburski@microsoft.wsei.edu.pl>
2022-06-28 20:27:17 +0200

    28068

--- lab_projekt/Windows/AddTruck.xaml.cs
+++ lab_projekt/Windows/AddTruck.xaml.cs
@@ -37,21 +37,6 @@ namespace lab_projekt
         }
         void OnClick1(object sender, RoutedEventArgs e)
         {
-            List<string> repairs = new List<string>();
-
-            repairs.Add("Wymina oleju silnikowego");
-            repairs.Add("Wymina oleju skrzyni biegów");
-            repairs.Add("Wymina oleju dyferencjału");
-            repairs.Add("Wymina opon przednich");
-            repairs.Add("Wymina opon tylnych");
-            repairs.Add("Wymina klocków hamulcowych przednich");
-            repairs.Add("Wymina klocków hamulcowych tylnych");
-            repairs.Add("Wymina tarcz hamulcowych przednich");
-            repairs.Add("Wymina tarcz hamulcowych tylnych");
-            repairs.Add("Wymina amortyzatorów przednich");
-            repairs.Add("Wymina poduszek tylnego zawieszenia");
-            repairs.Add("Wymina oleju silnikowego");
-
            using (ProjektDbContext db = new ProjektDbContext())
            {
                 string[] c = ComboBox.SelectedItem.ToString().Split(' ');
@@ -63,14 +48,6 @@ namespace lab_projekt
                     db.Trucks.Add(new Truck() { Plate = Plate.Text, Brand = Brand.Text, Model = Model.Text, VIN = VIN.Text, DriverId = d.Id, Insurance = DateTime.Now, TechReview = DateTime.Now, TachoLeg = DateTime.Now, });
 
                 }
-                /*var newTruck = (from t in db.Trucks where t.Plate == Plate.Text select t.Id);
-                foreach (var t in newTruck)
-                {
-                    for (int i = 0; i < repairs.Count; i++)
-                    {                       
-                        db.Repairs.Add(new Repair() { Name = repairs[i], TruckId = t, Date = DateTime.Now});
-                    }
-                }*/
                 db.SaveChanges();
            }
 
