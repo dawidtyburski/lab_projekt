4c738eba3542c404190cdad39eaf761dced90b82
Dawid Tyburski <Dawid.Tyburski@microsoft.wsei.edu.pl>
2022-06-28 20:22:58 +0200

    280620227

--- lab_projekt/Pages/MainPage.xaml.cs
+++ lab_projekt/Pages/MainPage.xaml.cs
@@ -29,7 +29,7 @@ namespace lab_projekt
             public string Plate { get; set; }
             public string BrandModel { get; set; }
             public string VIN { get; set; }
-            public ComboBox Driver { get; set; }
+            public string Driver { get; set; }
             public string DriverPhone { get; set; }
 
         }
@@ -45,20 +45,6 @@ namespace lab_projekt
         { 
             using (ProjektDbContext db = new ProjektDbContext())
             {
-                List<string> driversList = new List<string>();
-                var dr = from d in db.Drivers
-                         select d;
-                foreach (var d in dr)
-                {
-                    driversList.Add($"{d.Firstname} {d.Lastname}");
-
-                }
-                ComboBox1.ItemsSource = driversList;
-
-                ComboBox temp = new ComboBox();
-                temp.ItemsSource = driversList;
-                temp.value
-
                 var drivers = db.Trucks
                         .Include(b => b.Driver)
                         .ToList();
@@ -67,49 +53,9 @@ namespace lab_projekt
                 list.Clear();
                 foreach (var item in trucks)
                 {
-                    list.Add(new Header() { Plate = item.Plate, BrandModel = $"{item.Brand} {item.Model}", VIN = item.VIN, Driver = temp, DriverPhone = item.Driver.Phone });
+                    list.Add(new Header() { Plate = item.Plate, BrandModel = $"{item.Brand} {item.Model}", VIN = item.VIN, Driver = $"{item.Driver.Firstname} {item.Driver.Lastname}", DriverPhone = item.Driver.Phone });
                 }
                 dataGrid1.ItemsSource = list;
-            }
-        }
-        void dataGrid1_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
-        {
-            if (e.EditAction == DataGridEditAction.Commit)
-            {
-                var column = e.Column as DataGridBoundColumn;
-                if (column != null)
-                {
-                    var bindingPath = (column.Binding as Binding).Path.Path;
-                    if (bindingPath == "Przypisany Kierowca")
-                    {
-                        int rowIndex = e.Row.GetIndex();
-                        using (ProjektDbContext db = new ProjektDbContext())
-                        {
-                            var trucks = from t in db.Trucks
-                                      where t.Plate == list[dataGrid1.SelectedIndex].Plate
-                                      select t;
-
-                            string cb = null;
-                            for (int i = 0; i < this.dataGrid1.Columns.Count; i++)
-                            {
-                                ComboBox cb1 = dataGrid1.Columns[3].GetCellContent(dataGrid1.Items[i]) as ComboBox;
-                                cb = cb1.Text;
-                            }
-                            string[] cb1Text = cb.Split(' ');
-                            var id = (from d in db.Drivers where d.Firstname == cb1Text[0] && d.Lastname == cb1Text[1] select d);
-                            
-                            foreach(var d in id)
-                            {
-                                foreach (var t in trucks)
-                                {
-                                    t.DriverId = d.Id;
-                                }
-                            }
-                            db.SaveChanges();
-                            BuildHeader();
-                        }
-                    }
-                }
             }
         }
         void OnClick1(object sender, RoutedEventArgs e)
@@ -137,21 +83,41 @@ namespace lab_projekt
         }
         void OnClick3(object sender, RoutedEventArgs e)
         {
-            AddTruck.Visibility = Visibility.Hidden;
-            Grid.Visibility = Visibility.Visible;
-            btn1.IsEnabled = true;
-            btn2.IsEnabled = true;
+            
             using (ProjektDbContext db = new ProjektDbContext())
             {
                 string[] c = ComboBox.SelectedItem.ToString().Split(' ');
 
                 var id = (from d in db.Drivers where d.Firstname == c[0] && d.Lastname == c[1] select d);
 
-                foreach (var d in id)
+                if (db.Trucks.Any(o => o.Plate == Plate.Text))
                 {
-                    db.Trucks.Add(new Truck() { Plate = Plate.Text, Brand = Brand.Text, Model = Model.Text, VIN = VIN.Text, DriverId = d.Id, Insurance = DateTime.Now, TechReview = DateTime.Now, TachoLeg = DateTime.Now });
+                    var result = MessageBox.Show("Istnieje pojazd o takim nr rejestracyjnym", "wrong_plate", MessageBoxButton.OK);
+                    if (result == MessageBoxResult.OK)
+                    {
+                        Plate.Text = null;
+                    }
                 }
-                db.SaveChanges();
+                else
+                {
+                    foreach (var d in id)
+                    {
+
+                        db.Trucks.Add(new Truck() { Plate = Plate.Text, Brand = Brand.Text, Model = Model.Text, VIN = VIN.Text, DriverId = d.Id, Insurance = DateTime.Now, TechReview = DateTime.Now, TachoLeg = DateTime.Now });
+                    }
+                    db.SaveChanges();
+                    AddTruck.Visibility = Visibility.Hidden;
+                    Grid.Visibility = Visibility.Visible;
+                    btn1.IsEnabled = true;
+                    btn2.IsEnabled = true;
+
+                    Plate.Text = null;
+                    Brand.Text = null;
+                    Model.Text = null;
+                    VIN.Text = null;
+
+                }               
+                
             }
             BuildHeader();
         }
@@ -164,7 +130,7 @@ namespace lab_projekt
         }
         void OnClick5(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            System.Windows.Application.Current.Shutdown();
         }
         void OnClick6(object sender, RoutedEventArgs e)
         {
@@ -176,6 +142,10 @@ namespace lab_projekt
             {
                 db.Drivers.Add(new Driver() { Firstname = Firstname.Text, Lastname = Lastname.Text, Phone = Phone.Text });
                 db.SaveChanges();
+
+                Firstname.Text = null;
+                Lastname.Text = null;
+                Phone.Text = null;
             }
             ComboBox.Items.Clear();
         }
@@ -216,6 +186,25 @@ namespace lab_projekt
         }
         void Context_Delete(object sender, RoutedEventArgs e)
         {
+            if(dataGrid1 != null)
+            {
+                var index = dataGrid1.SelectedIndex;
+                {
+                    using (ProjektDbContext db = new ProjektDbContext())
+                    {
+                        var truck = from t in db.Trucks
+                                    where t.Plate == list[index].Plate
+                                    select t;
+
+                        foreach (var t in truck)
+                        {
+                            db.Remove(t);
+                        }
+                        db.SaveChanges();
+                        BuildHeader();
+                    }
+                }
+            }
         }
 
     }
